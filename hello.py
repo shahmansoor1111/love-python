e6f6ed4937344cb5460a80fb9e73f350985be263
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-09-12 12:41:15 +0500

    ok

---
+++ hello.py
@@ -0,0 +1,39 @@
+import random
+# # print("enter a number to check")
+# h = int(input())
+# # if(h>10):
+# #     print("you put greater number than 10")
+# # elif(h<10):
+# #     print("you put lower number than 10")
+# # else:
+# #     print("equal")
+# while(h>10):
+#     print("shahmansoor")
+#     h = h-1
+
+# rand = random.randint(1,20)
+# while(True):
+#     inp = int(input())
+#     if(inp<rand):
+#         print("you put lower number ")
+#     elif(inp>rand):
+#         print("you put greater number ")
+#     else:
+#         print("congrats its equal")
+       
+# rands = random.randint(1,20)  
+
+# while(True):
+#     inps = int(input())
+   
+#     if(inps<rands):
+#         print("lover")
+#     elif(inps>rands):
+#         print("greater")
+#     else:
+#         print("equal")
+        
+        
+   
+# for x in range (2,22,2):
+#     print(x)
